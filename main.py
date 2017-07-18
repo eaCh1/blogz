5f2219c7a94ef186cd629e30b614a6dbbecd2fbf
Eric Andrew Chunn <ericandrewchunn@gmail.com>
2017-07-17 20:32:30 -0700

    added user class

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = "some_secret"
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:teddybear@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:pizza@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
@@ -12,10 +12,22 @@ db = SQLAlchemy(app)
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    body = db.Column(db.String(max))
+    body = db.Column(db.String(999))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
     def __init__(self, title, body):
         self.title = title
         self.body = body
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30))
+    password = db.Column(db.String(30))
+    blogs = db.relationship('Blog', backref='user')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 
 @app.route('/blog')
@@ -25,7 +37,8 @@ def index():
     if query_param:
         #render individual blog post page, with list of posts
         posts = Blog.query.filter_by(id=query_param)
-        return render_template("post.html", posts=posts)
+        return render_template("post.html",
+                                posts=posts)
     else:
         #grab all posts in the database
         posts = Blog.query.all()
@@ -34,19 +47,21 @@ def index():
                                 title="Post Things!",
                                 posts=posts)
 
-
 @app.route('/newpost', methods=['POST', 'GET'])
 def add_post():
 
     if request.method == 'POST':
         post_title = request.form['post-title']
         post_body = request.form['text-area']
+
         if not is_title_empty(post_title) and not is_body_empty(post_body):
             post = Blog(post_title, post_body)
             db.session.add(post)
             db.session.commit()
+
             post_id = str(post.id)
-            return redirect('/blog?id=' + post_id)
+            owner_id = str(post.owner_id)
+            return redirect('/blog?id=' + post_id + '&post_id=' + owner_id)
         else:
             if is_title_empty(post_title):
                 flash("Please provide a title for your post")
@@ -64,7 +79,6 @@ def is_title_empty(post_title):
     if post_title != "":
         return False
     return True
-
 def is_body_empty(post_body):
     if post_body != "":
         return False
